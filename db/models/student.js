b69a2e1b14f48f0295f9c6aca9c6fdb4cff16a79
Scott Techau <scotttechau@gmail.com>
2018-11-18 17:57:29 -0800

    Setup Sequelize models

--- db/models/student.js
+++ db/models/student.js
@@ -1,8 +1,38 @@
-'use strict';
+"use strict";
 
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require("sequelize");
+const db = require("../db");
 
-const Student = db.define();
+const upperCaseFirst = string =>
+  string.charAt(0).toUpperCase() + string.slice(1);
+
+const Student = db.define(
+  "student",
+  {
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    }
+  },
+  {
+    hooks: {
+      beforeCreate: student => {
+        student.firstName = upperCaseFirst(student.firstName);
+        student.lastName = upperCaseFirst(student.lastName);
+      }
+    }
+  }
+);
 
 module.exports = Student;
